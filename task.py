c80daf2286a65b629d2fb8e6a7836f2852fd1585
Nikita Ryzhakov <43493666+NikitaRy@users.noreply.github.com>
2025-01-31 18:24:31 +0300

    Update task.py
    комментарии

--- task.py
+++ task.py
@@ -1,23 +1,23 @@
 class Node:
 
+    #Инициализируем вершины
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
-
+#В нотации Голова-хвост
 head = Node(1, Node(2, Node(3, Node(4))))
 
-
+# выводим от головы к хвосту в цикле, пока не упремся в нон
 def print_list(head, end='\n'):
     while head:
         print(head.data, end=' -> ' if head.next else '')
         head = head.next
     print(end=end)
 
-
 print_list(head)
 
-
+# по-сути, просто обмен значений следующий за головой, хвост, голова
 def reverse_list(head, tail=None):
     while head:
         head.next, tail, head = tail, head, head.next
